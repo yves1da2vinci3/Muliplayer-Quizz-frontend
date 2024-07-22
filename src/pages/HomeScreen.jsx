ac2af76b4741b1a57dbc7b0132209490d3017943
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-07-22 17:58:58 +0000

    add copy button for sharing room Id

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,172 +1,214 @@
-import React, { useEffect, useRef, useState } from 'react'
-import CodeImg from '../assets/QuizzImg.png'
-import Coding from '../assets/coding.png'
-import Math from '../assets/calculator.png'
-import sport from '../assets/sport.png'
-import story from '../assets/napoleon.png'
-import Anime from '../assets/otaku.png'
-import Mode from '../assets/dress.png'
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, Group, TextInput, LoadingOverlay } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
-import generateKey from '../utils/generateKey'
-import { useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development'
-function HomeScreen({socket}) {
+import React, { useEffect, useRef, useState } from "react";
+import CodeImg from "../assets/QuizzImg.png";
+import Coding from "../assets/coding.png";
+import Math from "../assets/calculator.png";
+import sport from "../assets/sport.png";
+import story from "../assets/napoleon.png";
+import Anime from "../assets/otaku.png";
+import Mode from "../assets/dress.png";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button, Group, TextInput, LoadingOverlay } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import generateKey from "../utils/generateKey";
+import { useNavigate } from "react-router-dom/dist/umd/react-router-dom.development";
+function HomeScreen({ socket }) {
   const Types = [
     {
-      id : 1,
-      name : "Programmation",
-      img : Coding
+      id: 1,
+      name: "Programming",
+      img: Coding,
     },
     {
-      id : 2,
-      name : "Mathematique",
-      img : Math
+      id: 2,
+      name: "Mathematics",
+      img: Math,
     },
     {
-      id : 3,
-      name : "Sport",
-      img : sport
+      id: 3,
+      name: "Sports",
+      img: sport,
     },
     {
-      id : 4,
-      name : "Anime",
-      img : Anime
+      id: 4,
+      name: "Anime",
+      img: Anime,
     },
     {
-      id : 5,
-      name : "Histoire",
-      img : story
+      id: 5,
+      name: "History",
+      img: story,
     },
     {
-      id : 6,
-      name : "Mode",
-      img : Mode
+      id: 6,
+      name: "Fashion",
+      img: Mode,
     },
-  ]
-  const inputRef = useRef(null)
-    const [opened, { open, close }] = useDisclosure(false);
-    const [NameOpen, { open :NameOpenFunc, close : NameCloseFunc }] = useDisclosure(true);
-    const [name,setName] = useState("")
-    const [roomId,setRoomId] = useState("")
-    const [visible, { toggle }] = useDisclosure(false);
-   const navigate = useNavigate()
-    const closeNameModal = () => { 
-      if(name.length == 0){
-        notifications.show({
-          title: 'Name input Feedback',
-          color : "red",
-          message: 'Please enter a name',
-        })
-      }else{
-        NameCloseFunc()
-      }
-      }
-      // Initialize Socket Handler
-     const HandlerRoomCreation = (type) => { 
-      toggle()
-      const RoomId = generateKey()
-      socket.emit('createRoom',{
-        name : name,
-        roomId : RoomId,
-        type : type,
-      })
-      socket.on("feedbackCreatingRoom",(data)=> {
-        if(data.status === "success"){
-          notifications.show({
-            title: 'Feeback About creating room',
-            color : "green",
-            message: 'Your room has been created',
-          })
-          localStorage.removeItem("roomId")
-          localStorage.removeItem("name")
-          localStorage.setItem("roomId",RoomId)
-          localStorage.setItem("name",name)
-          navigate("/waiting")
-        }
-      })
-      }
-      // Handle Join
-      const HandlerRoomJoin = () => {
-        console.log("click on join button");
-        socket.emit("joinRoom", {
-          roomId: inputRef.current.value,
-          name,
-        });
-      
-        socket.on("feedbackJoiningRoom", (data) => {
-          if (data.status === "success") {
-            console.log("working");
-            // Update the roomId state variable with the user input
-            setRoomId(inputRef.current.value);
-      
-            localStorage.removeItem("roomId");
-            localStorage.removeItem("name");
-            localStorage.setItem("roomId", inputRef.current.value); // Use inputRef.current.value here
-            localStorage.setItem("name", name);
-            navigate("/waiting");
-          } else {
-            notifications.show({
-              title: "Feedback About Joining room",
-              color: "red",
-              message: `${data.message}`,
-            });
-          }
-        });
-      };
-      
-  return (
-    <div className='h-screen font-[poppins] p-4 flex flex-col  md:grid md:grid-cols-2' >
-     
-      {/* Name */}
-      <Modal size="lg" title="Enter Your Name" opened={NameOpen}  centered >
-            <div className="h-[10rem] flex justify-between p-4 flex-col " >
-         <TextInput onChange={(e ) => setName(e.target.value)} placeholder='your name' />
-         <Button onClick={closeNameModal} className='bg-blue-400 h-[2.5rem] mt-4' >Save</Button>
-            </div>
-          </Modal>
-        {/* Choose quizz modal */}
-        <Modal size="lg" opened={opened} centered onClose={close} title="Choose the Quizz Category">
-        <LoadingOverlay visible={visible} overlayBlur={2} />
-        <div className='h-[24rem]   flex-col flex p-4  ' >
-                  {/* actions card */}
-                  <div className='flex gap-4  justify-center flex-wrap '> 
-      
-      {Types.map((type,index)=> (<div   key={type.name + "dsdsdsds"} onClick={()=> HandlerRoomCreation(type.id)}  className=' cursor-pointer rounded-lg hover:bg-blue-300 drop-shadow-md h-[10rem] gap-y-3 items-center flex-col justify-center flex p-2 w-[10rem] bg-white' >
-        <img src={type.img} className="h-[5rem] " />
-        <p className='text-center font-bold' >{type.name}</p>
-      </div>))}
-    
-    
-      </div>
-          </div>
-      </Modal>
-    {/* Information relative to the platform and the connection */}
-        <div className='h-full  items-start flex-col gap-y-6 pt-10 capitalize  justify-center p-4 flex'>
-            <h1 className='text-2xl font-semibold'>Welcome to your <span className='text-blue-500'>online Quizz multiplayer game</span>  </h1>
-            <p className='text-gray-500 tracking-tighter '>
-            Online Quizz Multiplayer Game is an exciting and interactive game that allows players to compete with each other in real-time. The game consists of a series of questions that test the players' knowledge in various categories such as history, science, pop culture, and more. Each player has a limited amount of time to answer each question, adding a sense of urgency to the game. The game is designed to be played with multiple players, allowing friends and strangers from all around the world to compete against each other. The leaderboard keeps track of each player's score, creating a competitive and engaging environment. The game is accessible through any device with an internet connection, making it easy for anyone to participate. The questions are randomly generated, ensuring that the game is always fresh and exciting. The game can be played for fun or with real prizes, adding another level of excitement for players. Overall, Online Quizz Multiplayer Game is an entertaining and educational experience that combines competition and knowledge in a fun and engaging way.
-            </p>
-            <div className='flex  flex-col md:flex-row h-20  gap-x-6 w-full items-center' >
-                {/* Create a create boutton sesssion */}
-            
-                <Button className='bg-blue-500 h-[3rem]' onClick={open}>Create a session</Button>
-                <h1>Or</h1>
-                {/* Join an  current   sesssion */}
-                <div className='flex flex-col md:flex-row items-center w-[27rem] gap-x-4    '>
-                    <input ref={inputRef}  className='border-b-2 border-green-400 bg-gray-50 outline-none px-2 flex-1 h-[2.5rem]' />
-                    <Button className='bg-blue-500 h-[3rem]' onClick={HandlerRoomJoin}>Join a session</Button>
-                </div>
-           
-            </div>
-        </div>
-    {/* Image */}
-    <div className='h-full items-center justify-center flex '>
-        <img src={CodeImg} />
-    </div>
+  ];
 
-</div>
-  )
+  const inputRef = useRef(null);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [NameOpen, { open: NameOpenFunc, close: NameCloseFunc }] =
+    useDisclosure(true);
+  const [name, setName] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [visible, { toggle }] = useDisclosure(false);
+  const navigate = useNavigate();
+  const closeNameModal = () => {
+    if (name.length == 0) {
+      notifications.show({
+        title: "Name input Feedback",
+        color: "red",
+        message: "Please enter a name",
+      });
+    } else {
+      NameCloseFunc();
+    }
+  };
+  // Initialize Socket Handler
+  const HandlerRoomCreation = (type) => {
+    toggle();
+    const RoomId = generateKey();
+    socket.emit("createRoom", {
+      name: name,
+      roomId: RoomId,
+      type: type,
+    });
+    socket.on("feedbackCreatingRoom", (data) => {
+      if (data.status === "success") {
+        notifications.show({
+          title: "Feeback About creating room",
+          color: "green",
+          message: "Your room has been created",
+        });
+        localStorage.removeItem("roomId");
+        localStorage.removeItem("name");
+        localStorage.setItem("roomId", RoomId);
+        localStorage.setItem("name", name);
+        navigate("/waiting");
+      }
+    });
+  };
+  // Handle Join
+  const HandlerRoomJoin = () => {
+    console.log("click on join button");
+    socket.emit("joinRoom", {
+      roomId: inputRef.current.value,
+      name,
+    });
+
+    socket.on("feedbackJoiningRoom", (data) => {
+      if (data.status === "success") {
+        console.log("working");
+        // Update the roomId state variable with the user input
+        setRoomId(inputRef.current.value);
+
+        localStorage.removeItem("roomId");
+        localStorage.removeItem("name");
+        localStorage.setItem("roomId", inputRef.current.value); // Use inputRef.current.value here
+        localStorage.setItem("name", name);
+        navigate("/waiting");
+      } else {
+        notifications.show({
+          title: "Feedback About Joining room",
+          color: "red",
+          message: `${data.message}`,
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="h-screen font-[poppins] p-4 flex flex-col  md:grid md:grid-cols-2">
+      {/* Name */}
+      <Modal size="lg" title="Enter Your Name" opened={NameOpen} centered>
+        <div className="h-[10rem] flex justify-between p-4 flex-col ">
+          <TextInput
+            onChange={(e) => setName(e.target.value)}
+            placeholder="your name"
+          />
+          <Button
+            onClick={closeNameModal}
+            className="bg-blue-400 h-[2.5rem] mt-4"
+          >
+            Save
+          </Button>
+        </div>
+      </Modal>
+      {/* Choose quizz modal */}
+      <Modal
+        size="lg"
+        opened={opened}
+        centered
+        onClose={close}
+        title="Choose the Quizz Category"
+      >
+        <LoadingOverlay visible={visible} overlayBlur={2} />
+        <div className="h-[24rem]   flex-col flex p-4  ">
+          {/* actions card */}
+          <div className="flex gap-4  justify-center flex-wrap ">
+            {Types.map((type, index) => (
+              <div
+                key={type.name + "dsdsdsds"}
+                onClick={() => HandlerRoomCreation(type.id)}
+                className=" cursor-pointer rounded-lg hover:bg-blue-300 drop-shadow-md h-[10rem] gap-y-3 items-center flex-col justify-center flex p-2 w-[10rem] bg-white"
+              >
+                <img src={type.img} className="h-[5rem] " />
+                <p className="text-center font-bold">{type.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Modal>
+      {/* Information relative to the platform and the connection */}
+      <div className="h-full  items-start flex-col gap-y-6 pt-10 capitalize  justify-center p-4 flex">
+        <h1 className="text-2xl font-semibold">
+          Welcome to your{" "}
+          <span className="text-blue-500">online Quizz multiplayer game</span>{" "}
+        </h1>
+        <p className="text-gray-500 tracking-tighter ">
+          Online Quizz Multiplayer Game is an exciting and interactive game that
+          allows players to compete with each other in real-time. The game
+          consists of a series of questions that test the players' knowledge in
+          various categories such as history, science, pop culture, and more.
+          Each player has a limited amount of time to answer each question,
+          adding a sense of urgency to the game. The game is designed to be
+          played with multiple players, allowing friends and strangers from all
+          around the world to compete against each other. The leaderboard keeps
+          track of each player's score, creating a competitive and engaging
+          environment. The game is accessible through any device with an
+          internet connection, making it easy for anyone to participate. The
+          questions are randomly generated, ensuring that the game is always
+          fresh and exciting. The game can be played for fun or with real
+          prizes, adding another level of excitement for players. Overall,
+          Online Quizz Multiplayer Game is an entertaining and educational
+          experience that combines competition and knowledge in a fun and
+          engaging way.
+        </p>
+        <div className="flex  flex-col md:flex-row h-20  gap-x-6 w-full items-center">
+          {/* Create a create boutton sesssion */}
+
+          <Button className="bg-blue-500 h-[3rem]" onClick={open}>
+            Create a session
+          </Button>
+          <h1>Or</h1>
+          {/* Join an  current   sesssion */}
+          <div className="flex flex-col md:flex-row items-center w-[27rem] gap-x-4    ">
+            <input
+              ref={inputRef}
+              className="border-b-2 border-green-400 bg-gray-50 outline-none px-2 flex-1 h-[2.5rem]"
+            />
+            <Button className="bg-blue-500 h-[3rem]" onClick={HandlerRoomJoin}>
+              Join a session
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* Image */}
+      <div className="h-full items-center justify-center flex ">
+        <img src={CodeImg} />
+      </div>
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
--- src/pages/WaitingRoom.jsx
+++ src/pages/WaitingRoom.jsx
@@ -1,77 +1,106 @@
-import { Avatar, Button } from '@mantine/core'
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development'
-import { QuizzContext } from '../context/QuizzContext'
+import { ActionIcon, Avatar, Button, CopyButton, Tooltip } from "@mantine/core";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom/dist/umd/react-router-dom.development";
+import { QuizzContext } from "../context/QuizzContext";
+import { IoCopyOutline } from "react-icons/io5";
 
-function WaitingRoom({socket}) {
-  const navigate = useNavigate()
-   const {SetRoom} =useContext(QuizzContext)
-  useEffect(()=>{
+function WaitingRoom({ socket }) {
+  const navigate = useNavigate();
+  const { SetRoom } = useContext(QuizzContext);
+  useEffect(() => {
     // Joinging waiting
-    socket.emit("joinWaitingRoom",{
-      roomId : localStorage.getItem("roomId")
-    })
+    socket.emit("joinWaitingRoom", {
+      roomId: localStorage.getItem("roomId"),
+    });
     // Feedback room
-    socket.on("feedbackWaiting",(data)=> {
-      if(data.roomId === localStorage.getItem("roomId")){
-        setRoom(data.room)
-      localStorage.setItem("room",JSON.stringify(data.room))
-        }
-    })
+    socket.on("feedbackWaiting", (data) => {
+      if (data.roomId === localStorage.getItem("roomId")) {
+        setRoom(data.room);
+        localStorage.setItem("room", JSON.stringify(data.room));
+      }
+    });
 
     // Listing to  room update
-    socket.on("updateWaitingRoom",(data)=> {
-      console.log("fhummm ",data)
-      if(data.roomId === localStorage.getItem("roomId")){
-      setRoom(data.room)
-      localStorage.setItem("room",JSON.stringify(data.room))
+    socket.on("updateWaitingRoom", (data) => {
+      console.log("fhummm ", data);
+      if (data.roomId === localStorage.getItem("roomId")) {
+        setRoom(data.room);
+        localStorage.setItem("room", JSON.stringify(data.room));
       }
-    })
+    });
     // Game Start
-    socket.on("gameStart",(data)=> {
-      if(data.roomId === localStorage.getItem("roomId")){
-       
-        navigate("/quizz")
+    socket.on("gameStart", (data) => {
+      if (data.roomId === localStorage.getItem("roomId")) {
+        navigate("/quizz");
       }
-    })
-
-
-  },[])
-  const [room,setRoom] =useState({participants : []})
+    });
+  }, []);
+  const [room, setRoom] = useState({ participants: [] });
 
   // Launch The Game
-  const LaunchGame = () => { 
-    socket.emit("gameStart",{
+  const LaunchGame = () => {
+    socket.emit("gameStart", {
       roomId: localStorage.getItem("roomId"),
-    })
+    });
+  };
 
-   }
- 
   return (
-    <div className='h-screen bg-slate-50 items-center gap-y-2 flex-col justify-center flex'>
-      <h1 className='font-semibold tracking-wider' >Waiting Room : <span className='text-blue-400' >{localStorage.getItem("roomId")} </span></h1>
-      {/* Participants */}
-      <div className='h-[40rem] w-[30rem] bg-white drop-shadow-lg shadow-md rounded-lg  flex  p-4 flex-col ' >
-        {/* Participants */}
-       <ul className='flex-1 overflow-y-scroll flex-col flex gap-y-8 list-none pt-10' >
-        {room?.participants.map((participan,index)=> (<li key={index.toString()} className='h-[4rem] p-2 flex-row border-b-2 flex items-center w-full  ' >
-          <Avatar size={30}  />
-          <div className='flex-1 flex-col ml-4 flex' >
-            <p className='font-semibold'>{participan.name}</p>
-          </div>
-        </li>))}
-        
-        
-      
-        
-       </ul>
-        {/* Button to go */}
-        { room ? room.adminName === localStorage.getItem("name") &&  <Button className='bg-blue-500 h-[3rem]' onClick={LaunchGame}>Launch the game</Button> : "" }
-       
+    <div className="h-screen bg-slate-50 items-center gap-y-2 flex-col justify-center flex">
+      <div>
+        <div className="flex flex-row items-center gap-x-3">
+          <h1 className="font-semibold tracking-wider">Waiting Room : </h1>
+          <CopyButton
+            value={`${localStorage.getItem("roomId")}`}
+            timeout={2000}
+          >
+            {({ copied, copy }) => (
+              <>
+                <h1 className="text-blue-500 font-bold">
+                  {localStorage.getItem("roomId")}
+                </h1>
+                <Tooltip
+                  label={copied ? "Copied" : "Copy"}
+                  withArrow
+                  position="right"
+                >
+                  <ActionIcon color={copied ? "teal" : "gray"} onClick={copy}>
+                    <IoCopyOutline className="cursor-pointer" />
+                  </ActionIcon>
+                </Tooltip>
+              </>
+            )}
+          </CopyButton>
+        </div>
+        <p className="text-center mb-3">Share the room id with your friends</p>
+      </div>
 
+      {/* Participants */}
+      <div className="h-[40rem] w-[30rem] bg-white drop-shadow-lg shadow-md rounded-lg  flex  p-4 flex-col ">
+        {/* Participants */}
+        <ul className="flex-1 overflow-y-scroll flex-col flex gap-y-8 list-none pt-10">
+          {room?.participants.map((participan, index) => (
+            <li
+              key={index.toString()}
+              className="h-[4rem] p-2 flex-row border-b-2 flex items-center w-full  "
+            >
+              <Avatar size={30} />
+              <div className="flex-1 flex-col ml-4 flex">
+                <p className="font-semibold">{participan.name}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {/* Button to go */}
+        {room
+          ? room.adminName === localStorage.getItem("name") && (
+              <Button className="bg-blue-500 h-[3rem]" onClick={LaunchGame}>
+                Launch the game
+              </Button>
+            )
+          : ""}
       </div>
     </div>
-  )
+  );
 }
 
-export default WaitingRoom
+export default WaitingRoom;
